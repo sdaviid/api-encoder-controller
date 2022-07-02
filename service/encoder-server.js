111f483117b60267f438004963a78a2ee886f407
sdaviid <sdaviid@gmail.com>
2022-07-02 16:46:21 -0300

    loose logic because 5k on a notebook is not enougth

--- service/encoder-server.js
+++ service/encoder-server.js
@@ -1,12 +1,18 @@
 const axios = require('axios');
 
 async function list_line(uri, access_token){
-  const response = await axios.get(uri, {
-    headers: {
-      'token': access_token
-    }
-  });
-  return response
+  try{
+    const response = await axios.get(`${uri}:55001/api/file/line`, {
+      headers: {
+        'token': access_token
+      },
+      timeout: 1000 * 5
+    });
+    return response
+  }catch(err){
+    console.log(err);
+    return false;
+  }
 }
 
 
